e6571cf4b7eea8044bf93d8d34cbc35c27f44440
Alex Bradley <alexandre.bradley@shopify.com>
2022-01-25 14:33:43 -0500

    use fakeFS and improve assertions

--- test/project_types/script/layers/infrastructure/metadata_repository_test.rb
+++ test/project_types/script/layers/infrastructure/metadata_repository_test.rb
@@ -3,6 +3,7 @@
 require "project_types/script/test_helper"
 
 describe Script::Layers::Infrastructure::MetadataRepository do
+  include TestHelpers::FakeFS
   let(:instance) { Script::Layers::Infrastructure::MetadataRepository.new(ctx: ctx) }
   let(:ctx) { TestHelpers::FakeContext.new }
 
@@ -10,35 +11,41 @@ describe Script::Layers::Infrastructure::MetadataRepository do
     subject { instance.get_metadata }
 
     describe "when metadata file is present and valid" do
+      let(:major_version) { "1" }
+      let(:minor_version) { "0" }
       let(:metadata_file_location) { "metadata.json" }
       let(:metadata_json) do
         JSON.dump(
           {
             schemaVersions: {
-              example: { major: "1", minor: "0" },
+              example: { major: major_version, minor: minor_version },
             },
           },
         )
       end
 
+      before do
+        File.write(metadata_file_location, metadata_json)
+      end
+
       it "should return a proper metadata object" do
-        File.expects(:read).with(metadata_file_location).once.returns(metadata_json)
-        ctx
-          .expects(:file_exist?)
-          .with(metadata_file_location)
-          .once
-          .returns(true)
-        subject
+        assert_instance_of Script::Layers::Domain::Metadata, subject
+        assert_equal major_version, subject.schema_major_version
+        assert_equal minor_version, subject.schema_minor_version
       end
 
       describe "when filename argument is provided" do
-        let(:metadata_file_location) { "build/metadata.json" }
-        subject { instance.get_metadata(metadata_file_location) }
+        let(:metadata_file_location_argument) { "build/metadata.json" }
+        subject { instance.get_metadata(metadata_file_location_argument) }
+
         it "should read that file" do
-          File.expects(:read).with(metadata_file_location).once.returns(metadata_json)
+          File
+            .expects(:read)
+            .with(metadata_file_location_argument)
+            .once.returns(metadata_json)
           ctx
             .expects(:file_exist?)
-            .with(metadata_file_location)
+            .with(metadata_file_location_argument)
             .once
             .returns(true)
           subject
